2e3f87e40d86dff874e0e6c519174716a2ae8a24
Charlie <704661485@qq.com>
2020-07-27 22:17:12 +0800

    2020-7-6-home

--- bluelog/forms.py
+++ bluelog/forms.py
@@ -1,7 +1,9 @@
 from flask_ckeditor import CKEditorField
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, HiddenField
+from wtforms.validators import DataRequired, Length, ValidationError, Email, Optional, URL
+
+from bluelog import Category
 
 
 class LoginForm(FlaskForm):
@@ -15,3 +17,25 @@ class PostForm(FlaskForm):
     category = StringField('Category', coerce=int,default=1)
     body = CKEditorField('Body',validators=[DataRequired()])
     submit = SubmitField()
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.category.choices = [(category.id, category.name)
+                                 for category in Category.query.order_by(Category.name).all()]
+class Category(FlaskForm):
+    name = StringField('Name',validators=[DataRequired(),Length(1,20)])
+    submit = SubmitField()
+    def validate_name(self,field):
+        if Category.query.filter_by(name=field.data).first():
+            raise ValidationError('Name already in use.')
+
+class CommentForm(FlaskForm):
+    author = StringField('Name', validators=[DataRequired(),Length(1,30)])
+    email = StringField('Email',validators=[DataRequired(),Email(),Length(1,30)])
+    site = StringField('Site',validators=[Optional(),URL(),Length(0,255)])
+    body = TextAreaField('Comment',validators=[DataRequired()])
+    submit =  SubmitField()
+
+class AdminComment(CommentForm):
+    author = HiddenField()
+    email = HiddenField()
+    site =  HiddenField()
